60b0a44a36044401c53c8910a2220aa84b969e36
auliaharvy <auliaharvy@gmail.com>
2020-07-30 13:38:05 +0700

    Pool Form Fix

--- app/Http/Controllers/PoolController.php
+++ app/Http/Controllers/PoolController.php
@@ -59,7 +59,8 @@ class PoolController extends Controller
     public function getpoolpallet()
     {
         $pool_pallet = Auth::user()->reference_pool_pallet_id;
-        if($pool_pallet==null){
+        $role = Auth::user()->role;
+        if($pool_pallet=='pooldli' && $role<5){
         $pool = DB::table('pool_pallet as a')
             ->join('organization as b', 'a.organization_id', '=', 'b.organization_id')
             ->select('a.*', 'b.organization_name')
